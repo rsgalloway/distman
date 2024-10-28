c73fe42e9bb31e9edaeb959c66cddf27ba74e006
Ryan Galloway <ryan@rsg.io>
2024-10-27 18:13:52 -0700

    bumps envstack to 0.5.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(os.path.join(here, "README.md")) as f:
 
 setup(
     name="distman",
-    version="0.1.0",
+    version="0.1.1",
     description="Super simple software distribution system",
     long_description=long_description,
     long_description_content_type="text/markdown",
