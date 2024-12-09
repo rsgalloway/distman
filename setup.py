8419db1a4a84f0dbca4c1797753ae87595702b67
Ryan Galloway <ryan@rsg.io>
2024-12-09 06:12:02 -0800

    update envstack version, bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(os.path.join(here, "README.md")) as f:
 
 setup(
     name="distman",
-    version="0.2.4",
+    version="0.3.0",
     description="Super simple file distribution",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -72,7 +72,7 @@ setup(
         ],
     },
     install_requires=[
-        "envstack==0.6.2",
+        "envstack==0.7.1",
         "GitPython==3.1.43",
     ],
     python_requires=">=3.8",
