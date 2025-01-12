d819d260acb9887c8ba2de414351754f4e38d337
Ryan Galloway <ryan@rsg.io>
2025-01-12 07:19:50 -0800

    updates version to 0.3.5

--- lib/distman/__init__.py
+++ lib/distman/__init__.py
@@ -35,7 +35,7 @@ distman distributes files and directories to versioned destinations.
 
 __author__ = "ryan@rsgalloway.com"
 __prog__ = "distman"
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 try:
     import envstack
