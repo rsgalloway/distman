9abfe516f4265f909c217f279c9b94a09a9cda30
Ryan Galloway <ryan@rsg.io>
2025-03-03 05:48:02 -0800

    update envstack version, bump version to 0.4.2

--- lib/distman/__init__.py
+++ lib/distman/__init__.py
@@ -35,7 +35,7 @@ distman distributes files and directories to versioned destinations.
 
 __author__ = "ryan@rsgalloway.com"
 __prog__ = "distman"
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 try:
     import envstack
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(os.path.join(here, "README.md")) as f:
 
 setup(
     name="distman",
-    version="0.4.1",
+    version="0.4.2",
     description="Super simple file distribution",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -72,7 +72,7 @@ setup(
         ],
     },
     install_requires=[
-        "envstack==0.7.4",
+        "envstack>=0.8.3",
         "GitPython==3.1.43",
     ],
     python_requires=">=3.8",
